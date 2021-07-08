7ef6477e81f02b033cf51492d28b66b75070f16b
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-08 10:12:05 -0400

    Day002: tipos de variaveis em Java

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day002/VariaveisJava.java
@@ -0,0 +1,23 @@
+package com.mateus.ahundredOfJavaBasic.day002;
+
+/**
+ * Java define os seguintes tipos de variáveis:
+ */
+public class VariaveisJava {
+	/**
+	 * Variáveis de instância (campos não estáticos)
+	 */
+	public String variavelDeInstancia;
+	
+	/**
+	 * Variáveis de classe (campos estáticos)
+	 */
+	public static int VariavelDeClasse;
+
+	/**
+	 * Variáveis locais
+	 */
+	public String variavelLocal(String variavelLocal) {
+		return variavelLocal = "Variaveis Locais";
+	}
+}
