23fd3ead9818bf0bd71d99870c6dcb57585fd948
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-09 09:27:53 -0400

    feat: nomeando variaveis

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day003/NomeandoVariaveis.java
@@ -0,0 +1,25 @@
+package com.mateus.ahundredOfJavaBasic.day003;
+
+/**
+ *	No Java Os nomes das variáveis ​​diferenciam maiúsculas de minúsculas,
+ *	pode ser qualquer identificador legal - uma sequência de comprimento ilimitado de letras
+ *  e dígitos Unicode, começando com uma letra, o cifrão " $" ou o caractere sublinhado " _".
+ *  A convenção, entretanto, é sempre começar os nomes das variáveis ​​com uma letra, não " $" ou " _".
+ *  Além disso, o caractere do cifrão, por convenção, nunca é usado!
+ *  Espaço em branco não é permitido.
+ */
+public class NomeandoVariaveis {
+	
+	String VariavelIniciandoMaiusculo;
+	String _variavelNumerica;
+	static final String VARIAVEL_STATICA = " ";
+	String $Var;
+	String VariavelComNomeGrandeParaMelhorCompreensao;
+	String Var_ComCaractere;
+	
+	/**
+	 * Observações:
+	 * Lembre-se também de que o nome escolhido não deve ser uma palavra - chave ou uma palavra reservada 
+	 */
+
+}
