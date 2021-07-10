860a095befa87ca3f35fd42821f9a05edfb1da61
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-10 09:19:42 -0400

    Hello Word

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day004/HelloWord.java
@@ -0,0 +1,15 @@
+package com.mateus.ahundredOfJavaBasic.day004;
+
+/**
+ * O famoso Olá Mundo no Java
+ */
+public class HelloWord {
+	
+	/** Metodo Pricipal a ser executado */
+	public static void main(String[] args) {
+		
+		/** comando para Imprimir no Console da IDE */
+		System.out.println("Hello Word!!!");
+		
+	}
+}
