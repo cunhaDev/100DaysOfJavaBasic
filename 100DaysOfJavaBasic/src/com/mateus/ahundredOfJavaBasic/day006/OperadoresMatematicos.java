238d586d5444b2a531b05d2dff6ab2f2b688f985
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-12 14:52:37 -0400

    feat: operadores matematicos

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day006/OperadoresMatematicos.java
@@ -0,0 +1,27 @@
+package com.mateus.ahundredOfJavaBasic.day006;
+
+/** Operadores Matematicos em Java */
+public class OperadoresMatematicos {
+	
+	public static void main(String[] args) {
+		
+		int A = 2;
+		int B = 2;
+		
+		/** Somando */
+		int resultado1 = A + B;
+		
+		/** Subtraindo */
+		int resultado4 = A - B;
+		
+		/** Dividindo */
+		int resultado2 = A / B;
+		
+		/** Multiplicando */
+		int resultado3 = A * B;
+		
+		/** Modulo ou Resto */
+		int resultado5 = A % B;
+
+	}
+}
