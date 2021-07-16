44bcc0ce94c40a903a302cd3df28156cceff42fb
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-16 15:46:01 -0400

    Estrutura de decisao Switch

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day010/EstruturaDeDecisaoSwitch.java
@@ -0,0 +1,36 @@
+package com.mateus.ahundredOfJavaBasic.day010;
+
+import java.util.Scanner;
+
+public class EstruturaDeDecisaoSwitch {
+
+	private static final String DiaSemana = "Quarta";
+
+	public static void main(String[] args) {
+
+		Scanner diaEscolha = new Scanner(System.in);
+		System.out.print("Informe um numero de 1 a 5: ");
+
+		int opcao = diaEscolha.nextInt();
+		
+		switch (opcao) {
+		case 1:
+			System.out.println("Você escolheu Segunda Feira");
+			break;
+		case 2:
+			System.out.println("Você escolheu Terça Feira");
+			break;
+		case 3:
+			System.out.println("Você escolheu Quarta Feira");
+			break;
+		case 4:
+			System.out.println("Você escolheu Quinta Feira");
+			break;
+		case 5:
+			System.out.println("Você escolheu Sexta Feira");
+			break;
+		default:
+			System.out.println("Número inválido");
+		}
+	}
+}
