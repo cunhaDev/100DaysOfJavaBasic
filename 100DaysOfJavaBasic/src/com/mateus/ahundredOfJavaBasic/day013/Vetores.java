95e1b3c336ef1d83afdc286146aea98cba3f0acf
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-21 18:30:40 -0400

    vetor

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day013/Vetores.java
@@ -0,0 +1,13 @@
+package com.mateus.ahundredOfJavaBasic.day013;
+
+/** Vetores são estruturas de dados que armazenam usualmente
+ *  uma quantidade fixa de dados de um certo tipo */
+public class Vetores {
+		
+		int Vetor[]; /** Declaração de um Vetor */
+		
+		int vetor[] = new int[10]; /** Alocando Espaço ou Definindo o Tamanho de um Vetor */
+		
+		/** Vetor com Dados */
+		String nomes[] = {"Juca Bala", "Maria da Silva", "Marcos Paqueta"};
+}
