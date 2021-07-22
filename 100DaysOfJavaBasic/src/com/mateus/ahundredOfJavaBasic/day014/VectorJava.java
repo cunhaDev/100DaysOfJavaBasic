0027e02beaa9c70a03264278fa30ce0c82518f87
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-22 19:27:38 -0400

    vector

---
+++ 100DaysOfJavaBasic/src/com/mateus/ahundredOfJavaBasic/day014/VectorJava.java
@@ -0,0 +1,17 @@
+package com.mateus.ahundredOfJavaBasic.day014;
+
+import java.util.Vector;
+
+/** Vector Java 1.0 */
+public class VectorJava {
+	public static void main(String[] args) {
+		
+		Vector<String> vetor = new Vector<>();
+	
+		vetor.add("oi");
+		
+		while (true) {
+			System.out.println(vetor);
+		}
+	}
+}
